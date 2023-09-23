e45571d273d8ac6435e59c74332ba388e34640fb
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-09-23 20:16:29 +0300

    task complete review how to optimize code

---
+++ 04_Data_Types_and_Variables_Exercise/10_Gladiator_Expenses.py
@@ -0,0 +1,30 @@
+lost_fights_count = int(input())
+
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+
+
+helmets_broken = 0
+swords_broken = 0
+shields_brakes = 0
+armors_broken = 0
+for fight in range(1, lost_fights_count + 1):
+    if fight % 2 == 0:
+        helmets_broken += 1
+
+    if fight % 3 == 0:
+        swords_broken += 1
+
+    if fight % 2 == 0 and fight % 3 == 0:
+        shields_brakes += 1
+
+        if shields_brakes % 2 == 0:
+            armors_broken += 1
+
+total_cost = helmets_broken * helmet_price \
+             + swords_broken * sword_price \
+             + shields_brakes * shield_price \
+             + armors_broken * armor_price
+print(f"Gladiator expenses: {total_cost:.2f} aureus")
