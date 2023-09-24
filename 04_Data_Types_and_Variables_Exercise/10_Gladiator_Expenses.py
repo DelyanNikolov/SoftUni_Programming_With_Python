46d459074b1783ad5925b8a6f280f010f9f3416e
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-09-24 21:58:02 +0300

    code updated with solution from exercise video dividing instead of if/else

--- 04_Data_Types_and_Variables_Exercise/10_Gladiator_Expenses.py
+++ 04_Data_Types_and_Variables_Exercise/10_Gladiator_Expenses.py
@@ -6,22 +6,22 @@ shield_price = float(input())
 armor_price = float(input())
 
 
-helmets_broken = 0
-swords_broken = 0
-shields_brakes = 0
-armors_broken = 0
-for fight in range(1, lost_fights_count + 1):
-    if fight % 2 == 0:
-        helmets_broken += 1
-
-    if fight % 3 == 0:
-        swords_broken += 1
-
-    if fight % 2 == 0 and fight % 3 == 0:
-        shields_brakes += 1
-
-        if shields_brakes % 2 == 0:
-            armors_broken += 1
+helmets_broken = lost_fights_count // 2
+swords_broken = lost_fights_count // 3
+shields_brakes = lost_fights_count // (2 + 3)
+armors_broken = shields_brakes // 2
+# for fight in range(1, lost_fights_count + 1):
+#     if fight % 2 == 0:
+#         helmets_broken += 1
+#
+#     if fight % 3 == 0:
+#         swords_broken += 1
+#
+#     if fight % 2 == 0 and fight % 3 == 0:
+#         shields_brakes += 1
+#
+#         if shields_brakes % 2 == 0:
+#             armors_broken += 1
 
 total_cost = helmets_broken * helmet_price \
              + swords_broken * sword_price \
