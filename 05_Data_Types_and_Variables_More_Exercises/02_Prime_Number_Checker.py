2b597139bbe8c5f84ba09a7136b6da1907d04f69
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-09-24 10:50:45 +0300

    Create 02_Prime_Number_Checker.py
    task complete

---
+++ 05_Data_Types_and_Variables_More_Exercises/02_Prime_Number_Checker.py
@@ -0,0 +1,8 @@
+number = int(input())
+is_prime = True
+if number > 1:
+    for num in range(2, number):
+        if number % num == 0:
+            is_prime = False
+            break
+print(is_prime)
