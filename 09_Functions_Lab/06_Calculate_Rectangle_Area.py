d62f1ca81df7298e44cfade3a1bae78a31543c15
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-10-05 19:17:08 +0300

    task complete

---
+++ 09_Functions_Lab/06_Calculate_Rectangle_Area.py
@@ -0,0 +1,10 @@
+def calculate_rectangle_rea(a, b):
+    """calculates area of rectangle with given
+    side_a, and side_b"""
+    return a * b
+
+
+side_a = int(input())
+side_b = int(input())
+
+print(calculate_rectangle_rea(side_a, side_b))
