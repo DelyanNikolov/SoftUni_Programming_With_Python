a1aa785a07e6722ed3966ede071a3fea29442d7c
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-10-11 21:13:30 +0300

    task complete

---
+++ 12_Lists_Advanced_Lab/03_To_do_List.py
@@ -0,0 +1,14 @@
+def sort_to_do_list():
+    to_do_notes = []
+    while True:
+        note = input()
+        if note == "End":
+            break
+
+        to_do_notes.append(note)
+    sorted_notes = sorted(to_do_notes, key=lambda x: int(x.split("-")[0]))
+    return [note.split("-")[1] for note in sorted_notes]
+
+
+result = sort_to_do_list()
+print(result)
