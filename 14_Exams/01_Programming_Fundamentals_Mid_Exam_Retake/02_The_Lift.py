261ab241acd2971c4fabb1d59deff6fc3d01d950
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-10-16 21:24:36 +0300

    added comments

--- 14_Exams/01_Programming_Fundamentals_Mid_Exam_Retake/02_The_Lift.py
+++ 14_Exams/01_Programming_Fundamentals_Mid_Exam_Retake/02_The_Lift.py
@@ -3,13 +3,13 @@ wagons_in_lift = [int(man) for man in input().split()]
 
 for wagon_number in range(len(wagons_in_lift)):
     space_in_wagon = wagons_in_lift[wagon_number]
-    if space_in_wagon > 4:
+    if space_in_wagon > 4:  # chek if the cart is full, if 4 people are in the cart --> check next wagon
         continue
     else:
-        if people_on_que >= 4:
-            people_to_board = 4 - space_in_wagon
-        else:
-            people_to_board = people_on_que
+        if people_on_que >= 4:                      # chek if people on the que are more of the capacity of the cart
+            people_to_board = 4 - space_in_wagon    # if there are more, we calculate how many people can fit
+        else:                                       # this check insures that we can't get negative people count in
+            people_to_board = people_on_que         # the que
         wagons_in_lift[wagon_number] += people_to_board
         people_on_que -= people_to_board
 
