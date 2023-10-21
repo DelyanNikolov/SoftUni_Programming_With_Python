aa923cd542bbc952c724014ac939d8b0f6040419
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-10-21 18:13:39 +0300

    task complete

---
+++ 14_Exams/02_Programming_Fundamentals_Mid_Exam/03_Numbers.py
@@ -0,0 +1,11 @@
+list_of_numbers = [int(num) for num in input().split()]
+average = sum(list_of_numbers) / len(list_of_numbers)
+top_nums = [top for top in list_of_numbers if top > average]
+top_nums.sort()
+top_nums = top_nums[::-1]
+top_five_nums = top_nums[:5]
+
+if top_nums:
+    print(" ".join(map(str, top_five_nums)))
+else:
+    print("No")
