f92cc2ee501d3ac7180d66b6efe58e7a2f44af7b
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-10-21 18:20:59 +0300

    task complete used sort(reverse=True) instead of slicing [::-1]

--- 14_Exams/02_Programming_Fundamentals_Mid_Exam/03_Numbers.py
+++ 14_Exams/02_Programming_Fundamentals_Mid_Exam/03_Numbers.py
@@ -1,8 +1,7 @@
 list_of_numbers = [int(num) for num in input().split()]
 average = sum(list_of_numbers) / len(list_of_numbers)
 top_nums = [top for top in list_of_numbers if top > average]
-top_nums.sort()
-top_nums = top_nums[::-1]
+top_nums.sort(reverse=True)
 top_five_nums = top_nums[:5]
 
 if top_nums:
