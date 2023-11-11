eb863049179f48d77b7c940078df9b875b50eaeb
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-11-11 20:59:46 +0200

    task complete

---
+++ 21_Text Processing_Exercise/03_Extract_File.py
@@ -0,0 +1,11 @@
+path = input().split("\\")
+file_name = ""
+file_extension = ""
+for item in path:
+    if "." in item:
+        file_data = item.split(".")
+        file_name = file_data[0]
+        file_extension = file_data[1]
+
+print(f"File name: {file_name}")
+print(f"File extension: {file_extension}")
