2fbf40c456a790d7254b7f5a5662236d554898d9
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-11-11 21:00:36 +0200

    task complete with lector code as comment

--- 21_Text Processing_Exercise/03_Extract_File.py
+++ 21_Text Processing_Exercise/03_Extract_File.py
@@ -9,3 +9,8 @@ for item in path:
 
 print(f"File name: {file_name}")
 print(f"File extension: {file_extension}")
+# filepath = input().split("\\")
+# filename_and_extension = filepath[-1]
+# filename, extension = filename_and_extension.split(".")
+# print(f"File name: {filename}")
+# print(f"File extension: {extension}")
