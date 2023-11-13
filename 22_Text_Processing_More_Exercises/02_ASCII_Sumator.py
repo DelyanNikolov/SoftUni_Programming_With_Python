e2d1c2fda37588fcc3bcf3d43af0ea69e0cb41e2
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2023-11-13 20:40:45 +0200

    task complete updated with better solution in comment

--- 22_Text_Processing_More_Exercises/02_ASCII_Sumator.py
+++ 22_Text_Processing_More_Exercises/02_ASCII_Sumator.py
@@ -1,11 +1,12 @@
 first_char = input()
-second_char = input()
+last_char = input()
 text = input()
 
 first_char_code = ord(first_char)
-second_char_code = ord(second_char)
+last_char_code = ord(last_char)
 ascii_sum = 0
+# ascii_sum = [ord(symbol) for symbol in text if ord(first_char) < ord(symbol) < ord(second_char)]
 for letter in text:
-    if first_char_code < ord(letter) < second_char_code:
+    if first_char_code < ord(letter) < last_char_code:
         ascii_sum += ord(letter)
 print(ascii_sum)
