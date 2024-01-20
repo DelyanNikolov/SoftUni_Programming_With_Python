8a569406fb21a7bd9e64564a81aa6d29c1a3eab1
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-01-20 18:14:00 +0200

    task complete

--- 03_Advansed/06_Multidimensional_Lists_Lab/07_Square_with_Maximum_Sum.py
+++ 03_Advansed/06_Multidimensional_Lists_Lab/07_Square_with_Maximum_Sum.py
@@ -10,13 +10,17 @@ for _ in range(rows):
 for row_idx in range(rows - square_side + 1):
     for col_idx in range(len(matrix[row_idx]) - square_side + 1):
         sub_matrix_total = 0
-        flatten_square = []
+        sub_matrix_elements = []
         for i in range(square_side):
-            flatten_square.extend(matrix[row_idx + i][col_idx:col_idx + square_side])
-        sub_matrix_total += sum(flatten_square)
+            row = matrix[row_idx + i][col_idx:col_idx + square_side]
+            sub_matrix_elements.append(row)
+            sub_matrix_total += sum(row)
         if max_square_sum < sub_matrix_total:
             max_square_sum = sub_matrix_total
-            max_square_numbers.append(flatten_square)
-print(*max_square_numbers[-1][:square_side])
-print(*max_square_numbers[-1][square_side:])
+            max_square_numbers.clear()
+            max_square_numbers.extend(sub_matrix_elements)
+
+
+for element in max_square_numbers:
+    print(*element)
 print(max_square_sum)
