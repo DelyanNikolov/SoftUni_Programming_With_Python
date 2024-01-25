6eed51494d479f895e24650474a4d77bcd31ecb0
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-01-25 12:22:52 +0200

    task complete correct solution

--- 03_Advansed/07_Multidimensional_Lists_Exercise_1/09_Miner.py
+++ 03_Advansed/07_Multidimensional_Lists_Exercise_1/09_Miner.py
@@ -1,55 +1,38 @@
 from collections import deque
 
-
-def collecting_field_info(field):
-    """traversing the matrix and collecting coordinates for: coal, end of route, miner start position"""
-    for row in range(n):
-        for col in range(n):
-            if field[row][col] == "c":
-                current_coordinates = (row, col)
-                coal_coordinates.append(current_coordinates)
-            elif field[row][col] == "e":
-                current_coordinates = (row, col)
-                end_of_route.append(current_coordinates)
-            elif field[row][col] == "s":
-                current_coordinates = (row, col)
-                start_position.append(current_coordinates)
-
-
-def moving_miner(actions):
-    command = moving_commands.popleft()
-    current_x, current_y = start_position[0]
-    x, y = commands[command]
-    new_x, new_y = current_x + x, current_y + y
-    new_position = (new_x, new_y)
-    if 0 <= new_x < n and 0 <= new_y < n:
-        if new_position in coal_coordinates:
-            coal_coordinates.remove(new_position)
-            matrix[new_x][new_y] = "*"
-        elif new_position in end_of_route:
-            print(f"Game over! ({new_x}, {new_y})")
-            exit()
-        else:
-            matrix[current_x][current_y], matrix[new_x][new_y] = matrix[new_x][new_y], matrix[current_x][current_y]
-
-
-# relatively change of coordinates of the miner for given moving command:
-commands = {
+directions = {
     "left": (0, -1),
     "right": (0, 1),
     "up": (-1, 0),
     "down": (1, 0)
 }
-coal_coordinates = []
-end_of_route = []
-start_position = []
-
+total_coal_in_matrix = 0
+coal_collected = 0
+matrix = []
+miner_pos = []
 n = int(input())  # square matrix with n rows and columns
 moving_commands = deque(command for command in input().split())  # collecting sequence of moving commands
-matrix = [[element for element in input().split()] for _ in range(n)]  # generating matrix with input info
+for row in range(n):  # generating matrix with input info
+    matrix.append(input().split())
+    if "s" in matrix[row]:
+        miner_pos = [row, matrix[row].index("s")]
+    total_coal_in_matrix += matrix[row].count("c")
 
-collecting_field_info(matrix)
-[print(*row) for row in matrix]
-print(" ")
-moving_miner(moving_commands)
-[print(*row) for row in matrix]
+while moving_commands:
+    command = moving_commands.popleft()
+    r = miner_pos[0] + directions[command][0]
+    c = miner_pos[1] + directions[command][1]
+    if 0 <= r < n and 0 <= c < n:
+        miner_pos = [r, c]
+        field = matrix[r][c]
+        if field == "c":
+            coal_collected += 1
+            matrix[r][c] = "*"
+            if coal_collected == total_coal_in_matrix:
+                print(f"You collected all coal! ({miner_pos[0]}, {miner_pos[1]})")
+                break
+        elif field == "e":
+            print(f"Game over! ({miner_pos[0]}, {miner_pos[1]})")
+            break
+else:
+    print(f"{total_coal_in_matrix - coal_collected} pieces of coal left. ({miner_pos[0]}, {miner_pos[1]})")
