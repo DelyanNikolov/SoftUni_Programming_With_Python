0cb997cdece5fb1cd5538e983f1f7b77aac4e0b2
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-02-22 16:03:15 +0200

    task complete

---
+++ Modules_Lab/mathematical_operations/helpers.py
@@ -0,0 +1,9 @@
+from Modules_Lab.mathematical_operations.core import add, subtract, divide, multiply, power
+
+sign_mapper = {
+    "+": add,
+    "-": subtract,
+    "/": divide,
+    "*": multiply,
+    "^": power
+}
