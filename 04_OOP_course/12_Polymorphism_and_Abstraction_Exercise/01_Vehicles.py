2fe19eeeb7d2ced5a0922aa75c80969d8365a7b7
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-03-12 21:20:32 +0200

    task complete

---
+++ 04_OOP_course/12_Polymorphism_and_Abstraction_Exercise/01_Vehicles.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    CONDITIONER_ON = 0.9
+
+    def drive(self, distance):
+        consumption = (self.fuel_consumption + self.CONDITIONER_ON) * distance
+        if consumption <= self.fuel_quantity:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    CONDITIONER_ON = 1.6
+    FUEL_TANK_FILL = 0.95
+
+    def drive(self, distance):
+        consumption = (self.fuel_consumption + self.CONDITIONER_ON) * distance
+        if consumption <= self.fuel_quantity:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * self.FUEL_TANK_FILL
+
+
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
