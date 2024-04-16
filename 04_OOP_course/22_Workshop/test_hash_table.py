bdd8eaed8e89d9de34481b700deaca337bee0e01
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-04-16 22:59:53 +0300

    added some unit tests

--- 04_OOP_course/22_Workshop/test_hash_table.py
+++ 04_OOP_course/22_Workshop/test_hash_table.py
@@ -32,5 +32,45 @@ class TestHashTable(TestCase):
         self.assertEqual(self.table._HashTable__keys, [None, "name", None, None])
         self.assertEqual(self.table._HashTable__values, [None, "Pesho", None, None])
 
+    def test_set_item_with_non_existing_key_expected_add_key_and_value(self):
+        self.table["name"] = "Pesho"
+
+        self.assertEqual(self.table._HashTable__keys, [None, "name", None, None])
+        self.assertEqual(self.table._HashTable__values, [None, "Pesho", None, None])
+
+    def test_set_item_with_non_existing_key_and_items_in_dict_expected_add_key_and_value(self):
+        self.table["name"] = "Pesho"
+        self.table["city"] = "Plovdiv"
+
+        self.assertEqual(self.table._HashTable__keys, [None, 'name', 'city', None])
+        self.assertEqual(self.table._HashTable__values, [None, "Pesho", "Plovdiv", None])
+
+    def test_set_item_with_non_existing_key_and_full_items_in_dict_expected_add_key_and_value_resize(self):
+        self.table["name"] = "Pesho"
+        self.table["city"] = "Plovdiv"
+        self.table["email"] = "P@abv.bg"
+        self.table["id"] = "123"
+        self.table["car"] = "Skoda"
+
+        self.assertEqual(self.table._HashTable__keys, ['email', 'name', 'city', 'id', None, None, 'car', None])
+        self.assertEqual(self.table._HashTable__values,
+                         ['P@abv.bg', 'Pesho', 'Plovdiv', '123', None, None, 'Skoda', None])
+
+        self.assertEqual(self.table.count, 5)
+        self.assertEqual(self.table.__len__(), 8)
+
+    def test_get_item_with_existing_key_expect_return_value(self):
+        self.table["name"] = "Pesho"
+
+        result = self.table.__getitem__("name")
+        self.assertEqual(result, "Pesho")
+
+    def test_get_item_with_non_existing_key_expect_return_error_msg(self):
+        error_msg = f"'Key does not exist!'"
+        with self.assertRaises(KeyError) as ke:
+            self.table.__getitem__("city")
+        self.assertEqual(error_msg, str(ke.exception))
+
+
 if __name__ == "__main__":
     main()
