664111d6eebf8d64ed2d7d42ce11d311440aed12
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-05-15 22:24:58 +0300

    task complete

---
+++ 05_Postgre_SQL/01_Data_Types_and_Table_Basics_Lab/02_Insert_Data_in_Table.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees(first_name, last_name, hiring_date, salary, devices_number)
+VALUES
+		('Pesho', 'Peshev', now(), 1200, 123456),
+		('Gosho', 'Peshev', now(), 1300, 123459),
+		('Georgi', 'Tonev', now(), 1500, 666666)
