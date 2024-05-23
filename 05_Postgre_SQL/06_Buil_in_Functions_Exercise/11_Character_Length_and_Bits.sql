a6772346382be51df0461b3d4b234030fc3c42e7
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-05-23 17:35:46 +0300

    task complete

---
+++ 05_Postgre_SQL/06_Buil_in_Functions_Exercise/11_Character_Length_and_Bits.sql
@@ -0,0 +1,9 @@
+SELECT
+	CONCAT(m.mountain_range, ' ', p.peak_name) AS mountain_information,
+	CHAR_LENGTH(CONCAT(m.mountain_range, ' ', p.peak_name)) AS characters_length,
+	BIT_LENGTH(CONCAT(m.mountain_range, ' ', p.peak_name)) AS bits_of_a_tring
+FROM
+	peaks AS p,
+	mountains AS m
+WHERE
+	m.id = p.mountain_id
