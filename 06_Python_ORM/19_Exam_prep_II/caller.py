5160e260c264a7013043a7dbbb355e380010d0f3
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-07-24 23:09:42 +0300

    added more solutions

--- 06_Python_ORM/19_Exam_prep_II/caller.py
+++ 06_Python_ORM/19_Exam_prep_II/caller.py
@@ -1,83 +1,90 @@
 import os
+from decimal import Decimal
 
 import django
-from django.db.models import Q, Count, F, When, Case, Value, BooleanField
+from django.db.models import Q, Count, F, Case, When, Value, BooleanField
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
 from main_app.models import Profile, Order, Product
 
 
-# Create queries within functions
 def get_profiles(search_string=None) -> str:
     if search_string is None:
         return ""
 
-    query = (Q(full_name__icontains=search_string)
-             | Q(email__icontains=search_string)
-             | Q(phone_number__icontains=search_string))
-
-    profiles = Profile.objects.filter(query).annotate(orders_count=Count('orders')).order_by('full_name')
+    profiles = Profile.objects.filter(
+        Q(full_name__icontains=search_string)
+            |
+        Q(email__icontains=search_string)
+            |
+        Q(phone_number__icontains=search_string)
+    ).order_by('full_name')
 
     if not profiles.exists():
         return ""
 
-    result = []
-    for p in profiles:
-        result.append(f"Profile: {p.full_name}, email: {p.email},"
-                      f" phone number: {p.phone_number}, orders: {p.orders_count}")
-    return '\n'.join(result)
+    return "\n".join(
+        f"Profile: {p.full_name}, email: {p.email}, phone number: {p.phone_number}, orders: {p.orders.count()}"
+        for p in profiles
+    )
 
 
 def get_loyal_profiles() -> str:
-    loyal_profiles = Profile.objects.get_regular_customers()
-    if not loyal_profiles.exists():
+    profiles = Profile.objects.get_regular_customers()
+
+    if not profiles.exists():
         return ""
 
-    return '\n'.join(f"Profile: {p.full_name}, orders: {p.orders_count}" for p in loyal_profiles)
+    return "\n".join(
+        f"Profile: {p.full_name}, orders: {p.orders.count()}"  # p.orders_count
+        for p in profiles
+    )
 
 
 def get_last_sold_products() -> str:
     last_order = Order.objects.prefetch_related('products').last()
+
     if last_order is None or not last_order.products.exists():
         return ""
-    order_products = last_order.products.order_by('name').values_list('name', flat=True)
-    return f"Last sold products: {', '.join(order_products)}"
+
+    # products = ', '.join([p.name for p in last_order.products.order_by('name')])
+    products = ', '.join(last_order.products.order_by('name').values_list('name', flat=True))
+
+    return f"Last sold products: {products}"
 
 
 def get_top_products() -> str:
     top_products = Product.objects.annotate(
         orders_count=Count('order')
     ).filter(
-        orders_count__gt=0
+        orders_count__gt=0,
     ).order_by(
-        '-orders_count', 'name'
+        '-orders_count',
+        'name'
     )[:5]
 
     if not top_products.exists():
         return ""
 
-    result = []
-    for p in top_products:
-        result.append(f"{p.name}, sold {p.orders_count} times")
+    product_lines = "\n".join(f"{p.name}, sold {p.orders_count} times" for p in top_products)
 
-    return f"Top products:\n{'\n'.join(result)}"
+    return f"Top products:\n" + product_lines
 
 
 def apply_discounts() -> str:
-    orders = Order.objects.annotate(
+    updated_orders_count = Order.objects.annotate(
         products_count=Count('products')
     ).filter(
         products_count__gt=2,
         is_completed=False
     ).update(
-        total_price=F('total_price') * 0.9
+        total_price=F('total_price') * 0.90
     )
 
-    return f"Discount applied to {orders} orders."
+    return f"Discount applied to {updated_orders_count} orders."
 
 
 def complete_order() -> str:
@@ -103,8 +110,3 @@ def complete_order() -> str:
     order.save()
 
     return "Order has been completed!"
-
-print(get_last_sold_products())
-print(get_top_products())
-print(apply_discounts())
-print(complete_order())
