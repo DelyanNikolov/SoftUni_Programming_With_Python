337e9372ab4f1059625f3f51b388330644303f07
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-08-13 22:29:24 +0300

    added category and fruit model, created all views and urls, some html inheritance

--- 06_Python_ORM/fruitipediaApp/fruitipediaApp/fruits/urls.py
+++ 06_Python_ORM/fruitipediaApp/fruitipediaApp/fruits/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 
 from fruitipediaApp.fruits import views
 
 urlpatterns = (
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('create-fruit/', views.create_vew, name='create_vew'),
+    path('create-category', views.create_category, name='create_category'),
+    path('<int:pk>/', include([
+        path('edit-fruit/', views.edit_view, name='edit_fruit'),
+        path('details-fruit/', views.delete_view, name='details_fruit'),
+        path('delete-fruit/', views.delete_view, name='delete_fruit'),
+    ]))
+
 )
