e3970e19a8fbe4d43c68e52660301c46434585f3
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-09-22 16:36:04 +0300

    added more functions to app

--- 07_Django_Basics/templateBasics/forumApp/forumApp/posts/urls.py
+++ 07_Django_Basics/templateBasics/forumApp/forumApp/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from forumApp.posts import views
+from forumApp.posts.views import index, dashboard
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
--- 07_Django_Basics/templateBasics/forumApp/forumApp/posts/views.py
+++ 07_Django_Basics/templateBasics/forumApp/forumApp/posts/views.py
@@ -5,9 +5,35 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-
     context = {
         "current_time": datetime.now().strftime("%I:%M %p"),
     }
 
     return render(request, 'base.html', context)
+
+
+def dashboard(request):
+    context = {
+        "posts": [
+            {
+                "title": "How to create a post 1",
+                "author": "Delyan Nikolov",
+                "content": "I need help creating a post",
+                "created_at": datetime.now().strftime("%I:%M %p"),
+            },
+            {
+                "title": "How to create a post 2",
+                "author": "",
+                "content": "I need help creating a post",
+                "created_at": datetime.now().strftime("%I:%M %p"),
+            },
+            {
+                "title": "How to create a post 3",
+                "author": "Delyan Nikolov",
+                "content": "",
+                "created_at": datetime.now().strftime("%I:%M %p"),
+            }
+        ]
+    }
+
+    return render(request, 'posts/dashboard.html', context)
