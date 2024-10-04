fddb76a5a16a1502e59bb2fd671511278f06faa8
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-10-04 21:12:55 +0300

    fix: fixed bug in bad language validator

--- 07_Django_Basics/templateBasics/forumApp/forumApp/posts/validators.py
+++ 07_Django_Basics/templateBasics/forumApp/forumApp/posts/validators.py
@@ -15,8 +15,8 @@ class BadLanguageValidator:
     def bad_words(self, value):
         if value is None or value == '':
             self.__bad_words = ["bad_word_1", "bad_word_2", "bad_word_3"]
-
-        self.__bad_words = value
+        else:
+            self.__bad_words = value
 
     def __call__(self, value):
         for bad_word in self.bad_words:
