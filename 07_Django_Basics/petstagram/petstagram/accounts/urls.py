e674d18f673d6cc0274dcf584ea72590bfd2cf24
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-10-09 21:10:21 +0300

    fix: updated templates with inheritance

--- 07_Django_Basics/petstagram/petstagram/accounts/urls.py
+++ 07_Django_Basics/petstagram/petstagram/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('profile/<int:pk>/', include([
-        path('', views.profile_edit, name='profile_edit'),
+        path('', views.profile_details, name='profile_details'),
         path('edit/', views.profile_edit, name='profile_edit'),
         path('delete/', views.profile_delete, name='profile_delete'),
     ]))
