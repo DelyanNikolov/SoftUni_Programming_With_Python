be7bba2cea6b50be8d8f4102f5898188ae389ee2
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-11-10 19:52:51 +0200

    feat: registered AppUser in admin with custom admin fields

--- 08_Django_Advanced/forumApp/forumApp/accounts/admin.py
+++ 08_Django_Advanced/forumApp/forumApp/accounts/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+from forumApp.accounts.forms import CustomUserChangeForm, CustomUserForm
+from forumApp.accounts.models import Profile
 
 # Register your models here.
 UserModel = get_user_model()
 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    fields = ('age', 'points')
+
+
 @admin.register(UserModel)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class AppUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+    form = CustomUserChangeForm
+    add_form = CustomUserForm
+
+    list_display = ('username', 'email',)
+
+    fieldsets = (
+        ('Credentials', {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
